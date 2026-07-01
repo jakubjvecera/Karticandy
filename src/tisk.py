93251c481f0d72ca4c2f128a8ad50c6a4e8eeaa5
Jakub Večeřa <100529442+jakubjvecera@users.noreply.github.com>
2026-07-01 20:55:54 +0200

    Set stdout and stderr to UTF-8 encoding
    Ensure stdout and stderr use UTF-8 encoding.

--- src/tisk.py
+++ src/tisk.py
@@ -13,6 +13,10 @@ import tkinter as tk
 from tkinter import messagebox, filedialog
 import shutil
 import io
+
+if sys.stdout.encoding != 'utf-8':
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8', errors='replace')
+    sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8', errors='replace')
  
 # ---------------- Cesta k projektu ----------------
 if len(sys.argv) < 2:
